5f1d18596efd3ef85534afcbc47959020a62eb75
kaur6907 <50712997+kaur6907@users.noreply.github.com>
2019-08-03 14:46:13 -0400

    Update Hand.java

--- src/game/Hand.java
+++ src/game/Hand.java
@@ -31,15 +31,7 @@ public class Hand {
         String str = "";
         for(Card c: cards){
             str+= c.toString()+"\n";
-            if(!c.isFaceUp){
-                allFaceUp=false;
-            }
-        }
-        // if all face up show total
-        if(allFaceUp){
-            str+="Total points = " + getTotal()+"\n";        
-        }
-        return str; 
+           
     }
     public void flipCard(){
         for(Card c : cards){
@@ -47,24 +39,7 @@ public class Hand {
         }
     }
     
-    //Return total points of a hand
-    public int getTotal(){
-        int totalPts = 0;
-         boolean hasAce = false;
-         //get total point; any aces by default = 1
-         for(int i=0; i<cards.size();i++){
-             totalPts+= cards.get(i).getRank();
-             //check to see if card is an ace
-             if(cards.get(i).printRank()=="Ace"){
-                 hasAce = true;
-             }
-             // make ace worth if total points <= 11
-             if(hasAce && totalPts <=11){
-                 totalPts += 10; //add 10 more i.e ace = 11
-             }
-         }
-        return totalPts;
-    }
+   
     
     public boolean give(Card card,Hand otherHand){
          if(!cards.contains(card)){
